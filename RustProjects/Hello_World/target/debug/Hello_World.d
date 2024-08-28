2e2ab51b2bfaeebc2c63938a9e9c83774e12229e
Soggy Genus <53271289+ColdWistler@users.noreply.github.com>
2024-08-28 08:05:30 +0000

    Add files via upload

---
+++ RustProjects/Hello_World/target/debug/Hello_World.d
@@ -0,0 +1,1 @@
+/home/bakasoggy/Code/RustProjects/Hello_World/target/debug/Hello_World: /home/bakasoggy/Code/RustProjects/Hello_World/src/main.rs
---
+++ RustProjects/Hello_World/target/debug/deps/Hello_World-baedbe14e196999e.d
@@ -0,0 +1,5 @@
+/home/bakasoggy/Code/RustProjects/Hello_World/target/debug/deps/libHello_World-baedbe14e196999e.rmeta: src/main.rs
+
+/home/bakasoggy/Code/RustProjects/Hello_World/target/debug/deps/Hello_World-baedbe14e196999e.d: src/main.rs
+
+src/main.rs:
