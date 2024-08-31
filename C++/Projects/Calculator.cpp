f5dcf7f063d0b636a46186867712692cd9691776
Soggy Genus <53271289+ColdWistler@users.noreply.github.com>
2024-08-31 13:16:38 +0000

    Add files via upload

--- C++/Projects/Calculator.cpp
+++ C++/Projects/Calculator.cpp
@@ -12,11 +12,25 @@ void addition(){
     cout << "Sum = " << sum << endl;
 }
 
+void subtraction(){
+    int x;
+    int y;
+    cout << "Enter Number: ";
+    cin >> x;
+    cout << "Enter Second Number: ";
+    cin >> y;
+    int sub = x - y;
+    cout << "Subtraction = " << sub << endl;
+}
+
+
 int main(){
     int opt;
     cout << "Enter Option: ";
     cin >> opt;
     if (opt == 1){
         addition();
+    }else if (opt == 2) {
+        subtraction();
     }
 }
