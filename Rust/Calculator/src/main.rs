9cb92f0a03e7a94d5bccea2c316aae580afb64ec
Soggy Genus <53271289+ColdWistler@users.noreply.github.com>
2024-09-03 09:29:47 +0000

    Add files via upload

--- Rust/Calculator/src/main.rs
+++ Rust/Calculator/src/main.rs
@@ -1,8 +1,10 @@
 fn main() {
     println!("Calculator:");
-    let opt = 1;
+    let opt = 2;
     if opt == 1{
         addition();
+    }else if opt == 2{
+        subtraction();
     }
 } 
 
@@ -12,4 +14,12 @@ fn addition() {
 
    let sum = x + y;
    println!("Sum = {}",sum);
+}
+
+fn subtraction() {
+    let x = 20;
+    let y = 30;
+    
+    let sub = x - y;
+    println!("Difference = {}",sub);
 }
