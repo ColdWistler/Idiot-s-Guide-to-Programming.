193ae79d0621413601354b365e65929130c42aa0
Soggy Genus <53271289+ColdWistler@users.noreply.github.com>
2024-09-04 17:05:11 +0000

    Add files via upload

--- Rust/Calculator/src/main.rs
+++ Rust/Calculator/src/main.rs
@@ -14,62 +14,50 @@ fn main() {
     io::stdin()
         .read_line(&mut stdopt)
         .expect("Failed to read line");
-    let num: i64 = stdopt.trim().parse().expect("Input not an integer.");
+    let num: i32 = stdopt.trim().parse().expect("Input not an integer.");
 
     println!("Enter second number");
     io::stdin()
         .read_line(&mut stdopt)
         .expect("Failed to read Line");
-    let num1: i64 = stdopt.trim().parse().expect("Input not an integer.");
+    let num1: i32 = stdopt.trim().parse().expect("Input not an integer.");
 
-    if opt == 0 {
+    if opt == 1 {
         addition(num, num1);
-    } else if opt == 1 {
-        subtraction(num, num1);
     } else if opt == 2 {
-        multiplication(num, num1);
+        subtraction(num, num1);
     } else if opt == 3 {
-        division(num, num1);
+        multiplication(num, num1);
     } else if opt == 4 {
+        division(num, num1);
+    } else if opt == 5 {
         remainder(num, num1);
     } else {
         print!("Invalid Option:");
     }
 }
 
-fn addition(num: i64, num1: i64) {
+fn addition(num: i32, num1: i32) {
     let sum = num + num1;
     println!("Sum = {}", sum);
 }
 
-fn subtraction() {
-    let x = 20;
-    let y = 30;
-
-    let sub = x - y;
+fn subtraction(num: i32, num1: i32) {
+    let sub = num - num1;
     println!("Difference = {}", sub);
 }
 
-fn multiplication() {
-    let x = 10;
-    let y = 20;
-
-    let prod = x * y;
+fn multiplication(num: i32, num1: i32) {
+    let prod = num * num1;
     println!("Product = {}", prod);
 }
 
-fn division() {
-    let x = 20;
-    let y = 10;
-
-    let div = x / y;
+fn division(num: i32, num1: i32) {
+    let div = num / num1;
     println!("Answer = {}", div);
 }
 
-fn remainder() {
-    let x = 20;
-    let y = 15;
-
-    let rem = x % y;
+fn remainder(num: i32, num1: i32) {
+    let rem = num % num1;
     println!("Remainder: {}", rem);
 }
