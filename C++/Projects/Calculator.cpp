5c7f0282ae37e6b5eb3894cf35c72b2e49156d60
Soggy Genus <53271289+ColdWistler@users.noreply.github.com>
2024-10-11 21:28:36 +0545

    Update Calculator.cpp

--- C++/Projects/Calculator.cpp
+++ C++/Projects/Calculator.cpp
@@ -26,7 +26,7 @@ void subtraction(){
 void multiplication(){
     int x;
     int y;
-    cout << ""
+    cout << ""p;
     cout << "Enter Number: ";
     cin >> x;
     cout << "Enter Second Number: ";
